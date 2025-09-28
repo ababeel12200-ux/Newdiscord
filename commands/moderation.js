eecbb3585ad442239e59d082e5addad7b2327cf2
Ismail Mansuri <ababeel12200@gmail.com>
2025-09-28 17:10:51 +0530

    Update moderation.js

--- commands/moderation.js
+++ commands/moderation.js
@@ -1,1 +1,49 @@
-
+module.exports = [
+  {
+    name: 'kick',
+    async execute(message, args) {
+      if (!message.member.permissions.has('KickMembers')) return message.reply('No permission');
+      const member = message.mentions.members.first();
+      if (!member) return message.reply('Mention user to kick');
+      if (!member.kickable) return message.reply('Cannot kick this user');
+      await member.kick();
+      message.channel.send(`${member.user.tag} was kicked.`);
+    }
+  },
+  {
+    name: 'ban',
+    async execute(message, args) {
+      if (!message.member.permissions.has('BanMembers')) return message.reply('No permission');
+      const member = message.mentions.members.first();
+      if (!member) return message.reply('Mention user to ban');
+      if (!member.bannable) return message.reply('Cannot ban this user');
+      await member.ban();
+      message.channel.send(`${member.user.tag} was banned.`);
+    }
+  },
+  {
+    name: 'mute',
+    async execute(message, args) {
+      if (!message.member.permissions.has('MuteMembers')) return message.reply('No permission');
+      const member = message.mentions.members.first();
+      if (!member) return message.reply('Mention user to mute');
+      let muteRole = message.guild.roles.cache.find(r => r.name === 'Muted');
+      if (!muteRole) {
+        try {
+          muteRole = await message.guild.roles.create({ name: 'Muted', permissions: [] });
+          message.guild.channels.cache.forEach(channel => {
+            channel.permissionOverwrites.edit(muteRole, {
+              SendMessages: false,
+              Speak: false,
+              AddReactions: false
+            });
+          });
+        } catch {
+          return message.reply('Failed to create Muted role.');
+        }
+      }
+      await member.roles.add(muteRole);
+      message.channel.send(`${member.user.tag} was muted.`);
+    }
+  }
+];
